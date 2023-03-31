22931f10aaa435b259f61fc4713ad11b24552f87
Filip_Mac <filiptankiewicz04@gmail.com>
2023-03-31 15:38:44 -0700

    opened on mac & ts working

--- App.tsx
+++ App.tsx
@@ -2,9 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const Message: Function = (message: string) => {
+    return message;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{Message("Welcome to EquityEagle")}</Text>
       <StatusBar style="auto" />
     </View>
   );
